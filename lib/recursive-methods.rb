7ca23c68c0ee73612087d4cf5ee04c758a3028bb
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-08 12:59:24 -0700

    First three recursive algorithms written and tests passing.

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,21 +1,37 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+    # base case is: return 1 if n = 0
+    # recursive case is: n * factorial(n-1)
+    if n < 0
+        raise ArgumentError
+    elsif n == 0
+        return 1
+    else
+        return n * factorial(n-1)
+    end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    # base case is: return "" for empty string
+    # recursive case: reverse what is left of string that had last element sliced off
+    newStr = s
+    return "" if newStr.length == 0
+    return newStr.slice!(-1) + reverse(newStr)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    # base case is: return empty string for length == 0
+    # recursive case: return reverse(s[0..-2])
+    return "" if s.length == 0 
+    return s[-1] + reverse(s[0..-2])
 end
 
 # Time complexity: ?
@@ -24,10 +40,33 @@ def bunny(n)
     raise NotImplementedError, "Method not implemented"
 end
 
+
 # Time complexity: ?
 # Space complexity: ?
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+    # for every (, we need a )
+    # base case would be the empty string or one set
+        # return true
+        # else return false
+    # recurse if not empty or one
+
+    # def mystery7(word)
+    #     if word.length < 2
+    #       return true
+    #     elsif word[0] != word[-1]
+    #       return false
+    #     else
+    #       puts word[1..-2]
+    #       return mystery7(word[1..-2])
+    #     end
+    #   end
+# ()
+
+    if s == ""
+        return true
+    else 
+        return nested(s)
+    end
 end
 
 # Time complexity: ?
