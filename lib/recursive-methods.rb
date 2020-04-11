37b7e8f66303028f3db51b7a1c28bbb50f50e54a
SuelyBarreto <suely@barreto.us>
2020-04-10 19:29:22 -0700

    time complexity and space complexity

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -6,7 +6,7 @@
 # e.g. fact(4) = 4 * 3 * 2 * 1 = 24, n = size of input
 # Linear Recursion - the algorithm is linear, running in O(n) time
 # Time Complexity : O(n)
-# Space Complexity: O(n)
+# Space complexity: O(n) maximum depth is proportional to n
 # The function is called recursively n times assuming both
 # decrementation and multiplication are constant operations.
 # Therefore, The time complexity of recursive factorial is O(n).
@@ -24,8 +24,8 @@ end
 # by reversing all letters and all words in the string
 # e.g. reverse("hello world") will return "dlrow olleh"
 # Linear Recursion
-# Time Complexity : O(n) runs n/2 times which is O(n)
-# Space Complexity: O(n)
+# Time complexity : O(n) n is the size of the string
+# Space complexity: O(n) maximum depth is proportional to n
 
 def reverse(str, left = 0, right = str.length-1)
   return str if left >= right # base case
@@ -42,15 +42,12 @@ end
 # reverses the string in place using a recursive algorithm
 # e.g. reverse("hello world") converts to "dlrow olleh"
 # Linear Recursion
-# Time Complexity : O(n) n/2 removes one from each side 
-# Space Complexity: O(n)
+# Time complexity : O(n) n is the size of the string
+# Space complexity: O(n) maximum depth is proportional to n
 
 def reverse_helper(s, first, last)
   if first < last
     s[first], s[last] = s[last], s[first]
-    # temp     = s[first]
-    # s[first] = s[last]
-    # s[last]  = temp
     return reverse_helper(s, first + 1, last - 1)
   else
     return s
@@ -68,8 +65,8 @@ end
 # a number of bunnies and each has two floppy ears. Compute
 # the total of ears recursively w/o loops or multiplication.
 # e.g. bunny(0)  = 0, bunny(1)  = 2, bunny(10) = 20
-# Time Complexity : O(1), constant time of 2 number of ears
-# Space Complexity: O(n)
+# Time Complexity : O(1) constant time of 2 number of ears
+# Space Complexity: O(1) maximum depth is 2
 
 def bunny(n, m = 2)
   return n if m == 1
@@ -84,8 +81,8 @@ end
 # Only parenthesis characters will be passed to this method.
 # e.g. nested("((()))") = true, nested("())") = false
 # Do this without creating new strings in the process.
-# Time Complexity : O(n) n/2 times operations
-# Space Complexity: O(n)
+# Time complexity : O(n) n is the size of the string
+# Space complexity: O(n) maximum depth is proportional to n
 
 def nested(s, left = 0, right = s.length - 1)
   return false if s.length % 2 != 0
@@ -100,8 +97,8 @@ end
 # accepts an unsorted array of integers and an integer to find
 # returns true if the value if found and false otherwise
 # Linear Recursion
-# Time Complexity : O(n) recursively till the length of the string
-# Space Complexity: O(n) no new data created
+# Time Complexity : O(n) n = size of the array
+# Space Complexity: O(n) maximum depth is proportional to n
 
 def search(arr, n, index = 0)
   return false if arr.size == 0
@@ -116,8 +113,8 @@ end
 # accepts a string and returns a boolean if a palindrome or not.
 # is_palindrome("racecar") = true, is_palindrome("smile") = false
 # Do this without creating new strings while solving the problem.
-# Time complexity : O(n)
-# Space complexity: O(n)
+# Time complexity : O(n) n is the size of the string
+# Space complexity: O(n) maximum depth is proportional to n
 # This recursive call is being done for at most n/2 times
 
 def is_palindrome(str, left = 0, right = str.length - 1)
@@ -127,7 +124,7 @@ def is_palindrome(str, left = 0, right = str.length - 1)
 end
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Write # 8 Digit_match(n, m) 
 # Recursive method accepts two positive integers as parameters
 # and returns the number of digits that match in both integers.
@@ -137,8 +134,8 @@ end
 #     | | | | | | | |
 #     6 2 5 3 0 8 4 1
 # returns 4 because 4 pairs match, 2-2, 5-5, 8-8, and 1-1.
-# Time complexity : O(n) 
-# Space complexity: O(n)
+# Time complexity : O(n) n is the number of digits of the smallest number
+# Space complexity: O(n) maximum depth is proportional to n
 
 def digit_match(int1, int2, count = 0, digit = 0)
   return 1     if int1 == 0 && int2 == 0 && digit == 0
@@ -156,7 +153,7 @@ end
 # Functions with two recursive calls are binary recursive functions
 # Binary Recursions return two calls of themselves
 # Time Complexity  : O(1.618ˆn) the tight upper bound of fibonacci
-# Space Complexity : O(n)
+# Space Complexity : O(n) maximum depth is proportional to n
 # [Phin – (phi)n] / Sqrt[5]
 # Phi = (1 + Sqrt[5]) / 2 the so-called golden mean
 # phi = (1 – Sqrt[5]) / 2 an associated golden number
