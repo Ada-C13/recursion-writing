67044f17cc8c33b95fd8d6e0d1781fe95c2cf251
denisseai <msdenisseanaya@gmail.com>
2020-04-11 21:56:22 -0700

    Added digit match

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,5 +1,7 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
+# source: https://www.bigocheatsheet.com/
+
 # Time complexity: O(n)
 # Space complexity: O(n)
 # Thank you to Chris-recursion video
@@ -35,20 +37,19 @@ end
 # Space complexity: O(n)
 def bunny(n)
     return 0 if n == 0
-    return 2 if n == 1
-    return 2 + bunny(n-1)
+    return 2 + bunny(n - 1)
 end
 
 # Time complexity: O(n)
 # Space complexity: O(1)
 # Talk to tutor
 def nested(s)
-    return true if s == ""
+    return true if s.empty?
     if s[0] == "(" && s[-1] == ")"
         s.slice!(0)
         s.slice!(-1)
         nested(s)
-    else
+    else 
         return false
     end
 end
@@ -70,14 +71,29 @@ def is_palindrome(s, i = 0, j = s.length - 1 )
     return true if i >= j
     s[i] == s[j] && is_palindrome(s, i + 1, j - 1)
 end
-    # Wanted somethign like this review w/tutor
-    # return true if s == ""
+# Wanted something like this review w/tutor
+    # return true if s.empty?
     # s[0] == s[-1] ? s = s[1..-2] : false
     # s.length > 1 ? is_palindrome(s) : true
 
+# Time complexity: O(n)
+# Space complexity: O (n)
+# resource: https://github.com/Creede15/practice-it/blob/master/chapter-12/digitMatch.java
+def digit_match(n, m, count = 0, run_match = true)
+    return 1 if n == 0 && m == 0 && run_match
+    return count if (n == 0) || (m == 0)
 
-# Time complexity: ?
-# Space complexity: ?
-def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+    count += 1 if (n % 10) == (m % 10)
+    return digit_match(n/10, m/10, count, false)
 end
+
+#ideas
+# def digit_match(n, m, match  = 0)
+#     while n != [] || m != []
+#         if m[-1] == n[-1]
+#             match += 1
+#         end
+#         n.pop
+#         m.pop
+#     end
+# end
