2e421be8c526511ec29c07b9f00ad13801619c52
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-04-11 20:06:39 -0700

    cleaned up and added time and spece complexity comments

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -17,13 +17,13 @@ end
 # Space complexity: O(n^2) because of the call stack and that each time we  are creating a new string
 def reverse(s)
   # raise NotImplementedError, "Method not implemented"
-  return s.slice(-1) + reverse(s.slice(0, (s.length - 1))) unless s.length.zero?
+  return s.slice(-1) + reverse(s.slice(0, s.length - 1)) unless s.length.zero?
   return "" if s.length.zero?
 end
 
 # Time complexity: O(n) wher n is n/2
 # Space complexity: O(n) wher n is n/2
-def reverse_inplace(s, first_index = 0, last_index = (s.length - 1))
+def reverse_inplace(s, first_index = 0, last_index = s.length - 1)
   # raise NotImplementedError, "Method not implemented"
   if first_index < last_index
     s[first_index], s[last_index] = s[last_index], s[first_index]
@@ -42,51 +42,63 @@ end
 
 # Time complexity: ?
 # Space complexity: ?
-def nested(s, first_index = 0, last_index = (s.length - 1))
+def nested(s, first_index = 0, last_index = s.length - 1)
   # raise NotImplementedError, "Method not implemented"
   return false if s.length.odd?
   return true if s.empty?
 
   if first_index < last_index
     if s[first_index] == "(" && s[last_index] == ")"
-        return nested(s, first_index + 1, last_index - 1)
-    else 
-        return false
+      return nested(s, first_index + 1, last_index - 1)
+    else
+      return false
     end
   end
   return true
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: worse case O(n) where n is length of array
+# Space complexity: worse case O(n) where n is length of array
 def search(array, value, current_index = 0 )
-    # raise NotImplementedError, "Method not implemented"
-    if current_index < array.length
-        if array[current_index] == value
-            return true
-        else 
-            return search(array, value, current_index += 1) 
-        end
+  # raise NotImplementedError, "Method not implemented"
+  if current_index < array.length
+    if array[current_index] == value
+      return true
+    else
+      return search(array, value, current_index + 1)
     end
-    return false
+  end
+  return false
 end
 
-# Time complexity: ?
-# Space complexity: ?
-def is_palindrome(s, first_index = 0, last_index = (s.length - 1))
+# Time complexity: O(n) where n i half the size of the string
+# Space complexity: O(n) where n i half the size of the string
+def is_palindrome(s, first_index = 0, last_index = s.length - 1)
   # raise NotImplementedError, "Method not implemented"
   if first_index < last_index
     if s[first_index] == s[last_index]
         return is_palindrome(s, first_index + 1, last_index - 1)
-    else 
+    else
         return false
     end
   end
   return true
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the length of the smaller number
+# Space complexity: O(n) where n is the length of the smaller number
 def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+  # raise NotImplementedError, "Method not implemented"
+  count = 0
+  if n < 10 && m < 10
+    count += 1 if n == m
+  elsif n < 10 
+    count += 1 if n == m % 10
+  elsif m < 10 
+    count += 1 if m == n % 10
+  elsif n >= 10 && m >= 10
+    count += 1 if n % 10 == m % 10
+    return digit_match(n/10, m/10) + count
+  end
+  count
 end
