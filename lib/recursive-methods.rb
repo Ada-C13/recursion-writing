8c1706b54c9813356bc439fd2d815483a7872e5f
Vera <lizeth.veraro@outlook.com>
2020-04-12 22:12:59 -0700

    Reverse in place, bunny(multiply x2), nested and search implementation

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -18,33 +18,85 @@ def reverse(s)
 
 end
 
-puts reverse("tac")
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n^2)
+
+def reverse_helper(string, start = 0, last = s.length - 1)
+  return string if string.length <= 1
+  return string if start == last
+  if start < last
+    string[start],string[last] = string[last], string[start]
+  end
+  return reverse_helper(string, start + 1, last - 1)
+end
+
+
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+    return reverse_helper(s, 0, s.length - 1)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+#   returns 2 for 1 bunny
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+  # raise NotImplementedError, "Method not implemented"
+  return 0 if n == 0
+  return 2 + bunny(n - 1)
+
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# case 2
+# bunny(1) --> 2 + 0 --> 2
+# case 4
+# bunny(1) = 2 + bunny(0) --> 2 + 0 + 2
+# bunny(2) = 2 + bunny(1) --> 2 + 2 + 4 
+# bunny(3) = 2 + bunny(2) --> 2 + 4 = 6
+# bunny(4) = 2 + bunny(3) --> 2 + 6 = 8
+
+# Time complexity: o(n)
+# Space complexity: o(n)
+
+def nested_helper(s, start = 0, last = s.length - 1)
+
+  while start < last
+    if s[start] != '(' || s[last] != ')'
+      return false
+    end
+    return nested_helper(s, start + 1, last - 1)
+  end
+
+  return true
+end
+
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+  return true if s.empty?
+  return false if s.length.odd?
+  return nested_helper(s)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n)
+def search_helper(array, value, index = 0)
+  while index < array.length
+    if array[index] == value
+      return true
+    end
+    return search_helper(array, value , index + 1)
+  end
+  return false
+end
+
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+  return false if array.empty?
+  return search_helper(array, value)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: o(n)
+# Space complexity: o(n)
 def is_palindrome(s)
     raise NotImplementedError, "Method not implemented"
 end
