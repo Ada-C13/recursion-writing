ff64d4540a2e807281ccc99f94fe84ba41b8f18c
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-13 15:36:58 -0700

    Corrected space complexities of methods with recursive calls on new strings or arrays, cleaned comments.

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,10 +1,6 @@
-# Authoring recursive algorithms. Add comments including time and space complexity for each method.
-
 # Time complexity: O(n)
 # Space complexity: O(n)
 def factorial(n)
-    # base case is: return 1 if n = 0
-    # recursive case is: n * factorial(n-1)
     if n < 0
         raise ArgumentError
     elsif n == 0
@@ -15,19 +11,16 @@ def factorial(n)
 end
 
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(n^2)
 def reverse(s)
-    # base case is: return "" for empty string
-    # recursive case: reverse what is left of string that had last element sliced off
-    newStr = s
-    return "" if newStr.length == 0
-    return newStr.slice!(-1) + reverse(newStr)
+    return "" if s.length == 0 
+    return s[-1] + reverse(s[0..-2])
 end
 
-
+# helper method for reverse_inplace()
+# Time complexity: O(n)
+# Space complexity: O(n)
 def recursive_swap(s, low, high)
-    # base case is: return s when high and low cross
-    # recursive case: return recursive_swap(s, low + 1, high - 1)
     if high <= low
         return s
     else
@@ -45,18 +38,13 @@ end
 # Time complexity: O(n)
 # Space complexity: O(n)
 def bunny(n)
-    # base case: return 0 when n == 0
-    # recursive case: return 2 + bunny(n-1)
     return 0 if n == 0
     return 2 + bunny(n-1)
 end
 
-
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(n^2)
 def nested(s)
-    # base case: empty string or one pair of parens
-    # recursive case: nested(s[1..-2])
     return false if s.length.odd?
 
     if s == "()" || s == ""
@@ -69,10 +57,8 @@ def nested(s)
 end
 
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(n^2)
 def search(array, value)
-    # base case: return false if array is empty
-    # recursive case: return search(array[1..-1], value)
     return false if array.empty?
 
     if array[0] == value
@@ -83,10 +69,8 @@ def search(array, value)
 end
 
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(n^2)
 def is_palindrome(s)
-    # base case: return true for empty string
-    # recursive case: return is_palindrome(s[1..-2])
     if s.length == 0
         return true
     elsif s[0] != s[-1]
@@ -98,40 +82,27 @@ end
 
 # Time complexity: O(log base 10 of n)
 # Space complexity: O(log base 10 of n)
-def digit_match(num1, num2)
-    # base case: return match count when length of either a or b is 0
-    # recursive case: return digit_match(a[0..-2], b[0..-2])
+def digit_match(num_1, num_2)
 
     # check ones place first
-    if num1 < 10 || num2 < 10
-        return num1 % 10 == num2 % 10 ? 1 : 0
+    if num_1 < 10 || num_2 < 10
+        return num_1 % 10 == num_2 % 10 ? 1 : 0
     end
 
     # if both nums > 10, check at least tens place
-    if num1 % 10 == num2 % 10
-        return 1 + digit_match(num1/10, num2/10)
+    if num_1 % 10 == num_2 % 10
+        return 1 + digit_match(num_1/10, num_2/10)
     end
 
-    return digit_match(num1/10, num2/10)
+    return digit_match(num_1/10, num_2/10)
 end
-
-# couldn't get the below to work?:
-# if a[-1] == nil || b[-1] == nil
-#     return 0
-# elsif a[-1] != b[-1]
-    # puts "Here's a[0..-2]: #{a[0..-2]}"
-#     return digit_match(a[0..-2], b[0..-2])
-# else
-#     return 1 + digit_match(a[0..-2], b[0..-2])
-# end
 
 
 # ADDED FUN
-# - fib sequence: (0 1 1 2 3 5 8 13 21 34)
-
+# fib sequence eg: (0 1 1 2 3 5 8 13 21 34)
+# Time complexity: O(n)
+# Space complexity: O(n)
 def fib(n)
-    # base case: return 0 if n == 0 OR if n == 1 return 1
-    # recursive case: return fib(n-2) + fib(n-1)
     if n < 0
         raise ArgumentError    
     elsif n == 0
