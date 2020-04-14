512796f1398cb7081501c8e9cc6cc5dcf3084694
Haben <haben7@gmail.com>
2020-04-13 21:50:09 -0700

    added methods up to Q  7

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -44,34 +44,84 @@ end
 # reverse_inplace(s) Write a method reverse_inplace that accepts a string as 
 # a parameter and then reverses the string in place using a recursive algorithm.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) 
+# Space complexity: O(n^2)
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    #base case
+   reverse_helper(s, 0, s.length - 1)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+def reverse_helper(s, first, last)
+ if s.length <= 1
+   return s
+ end
+
+ if first == last
+   return s
+ end
+
+ if first < last
+   s[first], s[last] = s[last], s[first]
+ end
+
+ reverse_helper(s, first + 1, last - 1)
+end
+
+# Time complexity: O(n)
+# Space complexity: O(n)
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    # base case
+    if n == 0
+        return 0
+    else
+        return 2 + bunny(n - 1)
+    end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length == 0
+      return true
+    end
+  
+    if s.length == 1
+      return false
+    end
+  
+    if s[0] == "(" && s[-1] == ")"
+      return nested(s[1..-2])
+    else
+        return false
+    end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+    if array.length == 0
+      return false
+    end
+  
+    if array[-1] == value
+      return true
+    else 
+      return search(array[0..-2], value)
+    end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length <= 1
+      return true
+    end
+  
+    if s[0] == s[-1]
+      return is_palindrome(s[1..-2])
+    else
+      return false
+    end
 end
 
 # Time complexity: ?
